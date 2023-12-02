37a96278721ed5ac89422bf6ebb5c58d484b9845
Houcine EL ADDALI <eladdali.lahoucine@ine.inpt.ma>
2023-12-02 01:59:32 +0100

    refactor tokenizer

---
+++ token/token_ctes.go
@@ -0,0 +1,34 @@
+package token
+
+/*
+*As the first iteration OUR JIPL lang
+* will define limited tokens so we declare em
+* as constants
+ */
+ const (
+	// SPECIALS
+	ILLEGAL   = -1 // unknown token
+	FILEENDED = 0  //file ended
+
+	//Identifiers and literals [1,20]
+	IDENTIFIER = 1 // variables
+	INT        = 2 // int values
+
+	//OPERATORS  values [40,80]
+	ASSIGN = 40
+	PLUS   = 41
+
+	//DELIMITERS [20,39]
+	CAMMA  = 20 // ,
+	SCOLON = 21 // ;
+
+	LP = 22 // (
+	RP = 23 // )
+
+	LCB = 24 // {
+	RCB = 25 // }
+
+	// KEYWORDS [100,110]
+	FUNCTION = 100
+	DEF      = 101 // a variable definition
+)
