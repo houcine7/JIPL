554fbb4bb0123c46db2452928e42b8ee9f96d749
Houcine EL ADDALI <eladdali.lahoucine@ine.inpt.ma>
2023-12-03 15:45:22 +0100

    feat: repel v0 to print tokens

---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/user"
+
+	repl "github.com/houcine7/JIPL/REPL"
+)
+
+func main() {
+	currUser, err := user.Current();
+
+	if err !=nil{
+		panic(err)
+	}
+
+	fmt.Printf("Hello %s!, welcome to JIPL happy coding :)",currUser.Username)
+	fmt.Printf("Start typing JIPL code ...\n")
+	repl.Start(os.Stdin,os.Stdout)
+}
